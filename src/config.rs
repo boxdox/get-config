45d22487eb16bb7b0edd7693ca6d888da5461b31
boxdox <19517445+boxdox@users.noreply.github.com>
2022-03-19 12:39:36 +0530

    added: load/store configuration

---
+++ src/config.rs
@@ -0,0 +1,44 @@
+use confy;
+use inquire::{error::InquireError, Text};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Config {
+    pub gist_id: Option<String>,
+    pub token: Option<String>,
+}
+
+impl ::std::default::Default for Config {
+    fn default() -> Self {
+        Self {
+            gist_id: None,
+            token: None,
+        }
+    }
+}
+
+pub fn get_config(package_name: &str) -> Result<Config, InquireError> {
+    let mut cfg: Config = confy::load(package_name).unwrap_or_default();
+
+    if cfg.gist_id.is_none() {
+        let gist_id =
+            Text::new("please enter a gist_id (this will be saved into a config file)").prompt()?;
+        cfg.gist_id = Some(gist_id);
+        let token = Text::new("enter a token for github (optional)")
+            .with_default("".into())
+            .prompt()?;
+        cfg.token = Some(token);
+        confy::store(package_name, &cfg).unwrap();
+    }
+
+    Ok(cfg)
+}
+
+pub fn clear_config(package_name: &str) -> Result<(), confy::ConfyError> {
+    let cfg = Config {
+        gist_id: None,
+        token: None,
+    };
+    confy::store(package_name, cfg)?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
+use std::env;
+use crate::config::{get_config, clear_config};
+
+mod config;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, world!");
+    let package_name = env!("CARGO_PKG_NAME");
+
+    // check if `init` is passed, if yes, reset the config
+    match env::args().nth(1) {
+        Some(arg) => {
+            if arg.eq("init") {
+                clear_config(package_name)?;
+            }
+        },
+        None => {}
+    };
+
+    let cfg = get_config(package_name)?;
+
+    dbg!(cfg);
+
     Ok(())
 }
